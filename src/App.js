be1d56c305c478420fd58d2b95aac9d28e3107c2
saar cohen <saar.cohenn@gmail.com>
2021-05-03 14:14:39 +0300

    Update for check

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Saar Cohen</h1>
-      <h2>I am a Full Stack Developer.</h2>
+      <h2>I am a Full Stack Developer...</h2>
       <img src={profilePicture} alt="Profile" />
     </div>
   );
